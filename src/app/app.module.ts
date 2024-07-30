99d7acad3144fa102227b1614f3472d886c670b5
Mohamed Aziz Belfaidi <azizbelfaidi.aziz@gmail.com>
2024-07-30 14:05:42 +0100

    contact+footer+callTo Action

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -8,6 +8,14 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { LayoutComponent } from './components/layout/layout.component';
 import { HeroSectionComponent } from './components/hero-section/hero-section.component';
 import { ButtonsComponent } from './components/buttons/buttons.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ContactInfoComponent } from './components/contact/contact-info/contact-info.component';
+import { SectionTitleComponent } from './components/section-title/section-title.component';
+import { ActionsComponent } from './components/actions/actions.component';
+
+
+
 
 @NgModule({
   declarations: [
@@ -16,13 +24,16 @@ import { ButtonsComponent } from './components/buttons/buttons.component';
     NavbarComponent,
     LayoutComponent,
     HeroSectionComponent,
-    ButtonsComponent
+    ButtonsComponent,
+    FooterComponent,
+    ContactComponent,
+    ContactInfoComponent,
+    SectionTitleComponent,
+    ActionsComponent,
+    
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
